82519b6bca9456a64324c826246b84590b174a7c
keevskipp <kevinadaircruzurtu@gmail.com>
2025-02-03 17:24:19 -0600

    prueba

---
+++ helloword.py
@@ -0,0 +1,58 @@
+#prueba
+print("hello word")
+
+#Ejercicio 1
+'¿Cual es la funcion de la memoria secundaria en una computadora?'
+'c) Almacenar informacion durante mucho tiempo, incluso despues de ciclos de apagado'
+'y encendido.'
+
+#Ejercicio 2
+'¿Que es un programa?'
+'secuencia de declaraciones o sentencias que han sido diseñadas para hacer algo'
+
+#Ejercicio 3
+'¿Cual es la diferencia entre un compilador y un interprete?'
+'El compilador traduce el codigo fuente a codigo maquina antes de ser ejecutado.'
+'El interprete traduce y ejecuta el codigo fuente en tiempo real.'
+
+#Ejercicio 4
+'¿Cual de los siguientes contiene “codigo maquina”?'
+'a) El interprete de Python.'
+
+#Ejercicio 5
+'¿Que esta mal en el siguiente codigo?:'
+>>> primt ’¡Hola, mundo!’
+File "<stdin>", line 1
+primt ’¡Hola, mundo!’^
+SyntaxError: invalid syntax>>>
+'El error esta en la palabra "primnt" ya que es print, tambien el que el mensaje no esta entre parentesis'
+
+#Ejercicio 6
+'¿En que lugar del computador queda almacenada una variable, como en este caso “x”,'
+'despues de ejecutar la siguiente linea de Python?:'
+x = 123
+'b) Memoria Principal.'
+
+#Ejercicio 7
+'¿Que mostrara en pantalla el siguiente programa?:'
+x = 43
+x = x + 1
+print(x)
+'b) 44.'
+
+#Ejercicio 8
+'Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana:'
+1. Unidad central de procesamiento.
+'El cerebro procesando informacion'
+2. Memoria principal.
+'La memoria a corto plazo almacenando datos temporales'
+3. Memoria secundaria.
+'La memoria a largo plazo almacenando datos permanentes'
+4. Dispositivos de entrada.
+'En el cuerpo pueden ser los sentidos ya que nos proporciona datos'
+5. Dispositivos de salida.
+'Pueden ser la boca ya que nos ayuda a comunicar los datos'
+
+#Ejercicio 9
+'¿Como puedes corregir un “Error de sintaxis”?'
+'Verificando el codigo que este bien escrito y leer los mensajes de error'
