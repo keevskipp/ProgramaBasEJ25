36b031f7412fbca5fa07f0c629e89f4ec69a3fa6
keevskipp <kevin.cruzu@uanl.edu.mx>
2025-02-21 03:58:03 +0000

    ;)

---
+++ Study/Inputs/Input_nombre.py
@@ -0,0 +1,6 @@
+#Pedirle un dato al usuario
+
+nombre = input("Cual es tu nombre?: ")
+
+#Mostrando el dato
+print(f'Tu nombre es: {nombre}')
---
+++ Study/Inputs/Input_numero.py
@@ -0,0 +1,13 @@
+#Pedirle un numero al usuario
+numero = input("Dame un numero: ")
+
+#Convirtiendo el numero entero y lo multiplico por 2
+resultado_entero = int(numero) * 2
+
+#Convirtiendo el numero entero y lo multiplico por 2
+resultado_float = float(numero) * 2
+
+#Convertir el numero float ingresado a int
+resultado_float_a_int = int(float(numero) * 2)
+
+print(resultado_float_a_int)
