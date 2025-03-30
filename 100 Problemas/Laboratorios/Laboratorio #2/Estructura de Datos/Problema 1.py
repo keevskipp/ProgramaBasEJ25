933040d5552106ea4d1c87bb18857182e8573ce2
keevskipp <kevin.cruzu@uanl.edu.mx>
2025-03-29 20:52:50 -0600

    prueba

---
+++ 100 Problemas/Laboratorios/Laboratorio #2/Estructura de Datos/Problema 1.py
@@ -0,0 +1,10 @@
+'''
+Ejercicio 1: An´alisis de Texto con Diccionarios y Conjuntos
+Desarrolla un programa que analice un texto ingresado por el usuario y determine:
+ El n´umero total de palabras en el texto.
+ La cantidad de palabras ´unicas, utilizando un conjunto para evitar repeticiones.
+ La frecuencia de cada palabra usando un diccionario donde la clave sea la
+palabra y el valor la cantidad de veces que aparece.
+ La palabra m´as frecuente y cu´antas veces aparece.
+El programa debe mostrar un resumen con estos datos
+'''
