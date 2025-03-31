b859008d685eef96ff6adc8871d67ba2ad4ba192
keevskipp <kevin.cruzu@uanl.edu.mx>
2025-03-30 19:47:47 -0600

    laboratorio 2

--- Laboratorios/Laboratorio #2/Problema 1.py
+++ Laboratorios/Laboratorio #2/Problema 1.py
@@ -1,4 +1,5 @@
 '''
+1.1 Estructuras de Datos
 Ejercicio 1: An´alisis de Texto con Diccionarios y Conjuntos
 Desarrolla un programa que analice un texto ingresado por el usuario y determine:
  El n´umero total de palabras en el texto.
